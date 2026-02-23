d335d8035044a7628a7cc00cdd97ed38af7faea1
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-02-23 17:18:33 +0100

    doc: add study comments

--- rust_network_monitor/src/main.rs
+++ rust_network_monitor/src/main.rs
@@ -4,15 +4,17 @@ use std::time::Instant;
 
 fn main() {
     // Ask OS what network interfaces exist
+    // Networks is a struct that holds the data of all network interfaces
     let mut networks = Networks::new_with_refreshed_list();
 
     // Hardcoded interface FOR TESTING
     let interface_name = "Ethernet 3";
 
     // Initial refresh
-    // Reset the data of all interfaces to 0, and ask OS to update them
+    // Refreshing the data to get the initial values for the selected interface
     networks.refresh();
 
+    // Get the initial received and transmitted bytes for the selected interface
     let mut previous_rx = networks
         .get(interface_name)
         .expect("Interface not found")
@@ -23,7 +25,10 @@ fn main() {
         .expect("Interface not found")
         .transmitted();
 
+    // Get the time we got the data
     let mut previous_time = Instant::now();
+
+
 
     loop  {
         thread::sleep(Duration::from_secs(1));
@@ -36,12 +41,16 @@ fn main() {
             .get(interface_name)
             .expect("Interface not found");
 
+        // Recalculate the received and transmitted bytes for the selected interface
         let current_rx = data.received();
         let current_tx = data.transmitted();
 
+        // Calculate the difference in received and transmitted bytes since the last check
+        // Use saturating_sub to avoid negative values in case of counter reset (underflow)
         let delta_rx = current_rx.saturating_sub(previous_rx);
         let delta_tx = current_tx.saturating_sub(previous_tx);
 
+        // Calculate the download and upload speeds in bytes per second
         let rx_per_sec = delta_rx as f64 / elapsed;
         let tx_per_sec = delta_tx as f64 / elapsed;
 
