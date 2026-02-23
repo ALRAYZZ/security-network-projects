326f5a5e2101f64eef0245528a290736169d5d48
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-02-23 17:48:26 +0100

    feat: added smoothing on the output - Helper method to change unit output based on size

--- rust_network_monitor/src/main.rs
+++ rust_network_monitor/src/main.rs
@@ -1,6 +1,19 @@
 use sysinfo::{Networks, System};
 use std::{thread, time::Duration};
+use std::collections::VecDeque;
 use std::time::Instant;
+
+// Change unit based on size of the speed
+fn human_readable(bytes_per_sec: f64) -> String {
+    if bytes_per_sec > 1024.0 * 1024.0 {
+        format!("{:.2} MB/s", bytes_per_sec / (1024.0 * 1024.0))
+    } else if bytes_per_sec > 1024.0 {
+        format!("{:.2} KB/s", bytes_per_sec / 1024.0)
+    } else {
+        format!("{:.2} B/s", bytes_per_sec)
+    }
+}
+
 
 fn main() {
     // Ask OS what network interfaces exist
@@ -29,6 +42,10 @@ fn main() {
     let mut previous_time = Instant::now();
 
 
+    // Rolling buffer for smoothing the speed values
+    let mut rx_history: VecDeque<f64> = VecDeque::with_capacity(5);
+    let mut tx_history: VecDeque<f64> = VecDeque::with_capacity(5);
+
 
     loop  {
         thread::sleep(Duration::from_secs(1));
@@ -47,15 +64,21 @@ fn main() {
 
         // Calculate the difference in received and transmitted bytes since the last check
         // Use saturating_sub to avoid negative values in case of counter reset (underflow)
-        let delta_rx = current_rx.saturating_sub(previous_rx);
-        let delta_tx = current_tx.saturating_sub(previous_tx);
+        let delta_rx = current_rx.saturating_sub(previous_rx) as f64 / elapsed;
+        let delta_tx = current_tx.saturating_sub(previous_tx) as f64 / elapsed;
 
-        // Calculate the download and upload speeds in bytes per second
-        let rx_per_sec = delta_rx as f64 / elapsed;
-        let tx_per_sec = delta_tx as f64 / elapsed;
+        // Add to history
+        if rx_history.len() == 5 { rx_history.pop_front(); }
+        if tx_history.len() == 5 { tx_history.pop_front(); }
+        rx_history.push_back(delta_rx);
+        tx_history.push_back(delta_tx);
 
-        println!("Download: {:.2} bytes/s", rx_per_sec);
-        println!("Upload: {:.2} bytes/s", tx_per_sec);
+        // Calculate average speed over the history
+        let rx_avg: f64 = rx_history.iter().sum::<f64>() / rx_history.len() as f64;
+        let tx_avg: f64 = tx_history.iter().sum::<f64>() / tx_history.len() as f64;
+
+        println!("Download: {}", human_readable(rx_avg));
+        println!("Upload: {}", human_readable(tx_avg));
         println!("---------------------------------");
 
         previous_rx = current_rx;
