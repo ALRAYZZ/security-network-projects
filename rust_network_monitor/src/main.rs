cf3ed614a0b111961c4b5cb9035534db667eb460
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-02-23 16:39:11 +0100

    feat: add collector layer for all network interfaces

--- rust_network_monitor/src/main.rs
+++ rust_network_monitor/src/main.rs
@@ -1,3 +1,21 @@
+use sysinfo::{Networks, System};
+use std::{thread, time::Duration};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut networks = Networks::new_with_refreshed_list();
+
+    loop  {
+        networks.refresh();
+
+        for (interface_name, data) in &networks {
+            println!("Interface: {}", interface_name);
+            println!("  Received: {} bytes", data.received());
+            println!("  Transmitted: {} bytes", data.transmitted());
+        }
+
+        println!("---------------------------------");
+
+        thread::sleep(Duration::from_secs(1));
+    }
 }
